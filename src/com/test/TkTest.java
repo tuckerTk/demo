a3629e98887eac58f86cced72115bf3e4aa388ca
Administrator <Administrator@tiankui-PC>
2015-05-20 20:01:01 +0800

    第一次提交到本地

---
+++ src/com/test/TkTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class TkTest {
+	public static void main(String[] args) {
+		try {
+			String a = null;
+			String[] ss= new String[]{"qqq","gggg"};
+			//System.out.println(a.equals(""));
+			System.out.println(ss[3]);
+		} catch (NullPointerException re) {
+			System.out.println("NullPointerException");
+		} catch (IndexOutOfBoundsException ie){
+			System.out.println("IndexOutOfBoundsException");
+			return;
+		}finally{
+			System.out.println("finally");
+		}
+	}
+
+}
