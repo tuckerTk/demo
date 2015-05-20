81f3ff2cedc2817cb3e9f9b34afc9eb4ea29dc2b
Administrator <Administrator@tiankui-PC>
2015-05-20 20:18:33 +0800

    gf

--- src/com/test/TkTest.java
+++ src/com/test/TkTest.java
@@ -3,7 +3,7 @@ package com.test;
 public class TkTest {
 	public static void main(String[] args) {
 		try {
-			String a = null;
+			String a = "";
 			String[] ss= new String[]{"qqq","gggg"};
 			//System.out.println(a.equals(""));
 			System.out.println(ss[3]);
